9cd4bb675a099dba611d6fde1ec7dce3fa37541d
Koki Nakamura <nk.levia22@gmail.com>
2022-02-23 15:37:18 +0900

    fix url in the setting info

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="original python utility",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/koki-nakamura22/pyutil",
+    url="https://github.com/koki-nakamura22/pyutil/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.8",
